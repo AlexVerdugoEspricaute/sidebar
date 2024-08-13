c58943a5c76b499ca5ec123bd772036bbfebe849
AlexVerdugo <alex.verdugo.-@hotmail.com>
2024-08-13 15:56:06 -0400

    Sidebar Terminado con modo white/black

--- script.js
+++ script.js
@@ -9,7 +9,17 @@ const body = document.querySelector("body"),
         sidebar.classList.toggle("close");
     });
 
+    searchBtn.addEventListener('click', ()=>{
+        sidebar.classList.remove("close");
+    });
+
     modeSwitch.addEventListener('click', ()=>{
         body.classList.toggle("dark");
+
+        if(body.classList.contains("dark")){
+            modeText.innerText = "Light Mode";
+        } else{
+            modeText.innerHTML = "Dark Mode";
+        }
     });
 
